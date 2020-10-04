32e650825ee792a29571b15d95cc928a976a3257
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-04 05:42:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,9 +1,7 @@
 require 'pry'
 
-def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
-end
-
+star_date = (rand(100000) + 400000) / 10.0
+binding.pry
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
