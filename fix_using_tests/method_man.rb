3f88ad4105341c5e82e906b11b2e759b5ccd9815
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-04 04:55:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,7 +3,10 @@ require 'pry'
 player1 = "Rocky"
 player2 = "Bullwinkle"
 
-puts "Hello + player1 + and + player2"
+def start_game
+  puts "Hello + player1 + and + player2"
+end 
 
-def play_game
-  puts "#{player2} is better than #{player1}" 
+#def play_game
+  #puts "#{player2} is better than #{player1}" 
+#end
