e2f2c349ae508bfcf288261bbdc78f545219785e
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-04 05:12:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,7 +4,7 @@ player1 = "Rocky"
 player2 = "Bullwinkle"
 
 def start_game("Rocky", "Bullwinkle")
-  puts "Hello Rocky and #Bullwinkle"
+  puts "Hello Rocky and Bullwinkle"
 end 
 
 def play_game(player1, player2)
