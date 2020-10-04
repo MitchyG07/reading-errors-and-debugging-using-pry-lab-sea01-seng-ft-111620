bd1fc6465b103fb47cede2ca2ed65e8938fafd1f
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-04 05:18:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,9 +4,12 @@ player1 = "Rocky"
 player2 = "Bullwinkle"
 
 def start_game(player1, player2)
-  p "Hello #{player1} and #{player2}"
+  "Hello #{player1} and #{player2}"
 end 
 
 def play_game(player1, player2)
-  p "#{player2} is better than #{player1}" 
+  "#{player2} is better than #{player1}" 
 end
+
+puts start_game
+puts play_game
